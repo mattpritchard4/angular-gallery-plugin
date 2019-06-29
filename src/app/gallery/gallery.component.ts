ddad8492b4473f098291b3936707d4e7880f24bf
Matt Pritchard <t3hsauce@skynet>
2019-06-29 19:10:03 -0400

    adds data service, injects feed.json into gallery component

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { DataService } from '../data.service';
 
 @Component({
     selector: 'my-gallery',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-    constructor() { }
+    @Input() feed: string;
+
+    images$: Object;
+
+    constructor(private data: DataService) { }
 
     ngOnInit() {
+        this.data.getImages().subscribe(
+            data => this.images$ = data
+        );
     }
 
 }
