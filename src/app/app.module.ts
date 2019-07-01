6b50608078435fc22061f0c329267c890399bb40
Matt Pritchard <mattpritchard4@gmail.com>
2019-07-01 09:22:16 -0400

    adds search pipe and optional search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,9 @@ import { GalleryComponent } from './gallery/gallery.component';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        NgxPaginationModule
+        NgxPaginationModule,
+        Ng2SearchPipeModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
