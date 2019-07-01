e02979bedf49cb4f523caabdce2c5480f8bc46df
Matt Pritchard <t3hsauce@skynet>
2019-06-30 22:34:31 -0400

    adds modal with working controls

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -18,9 +18,12 @@ export class GalleryComponent implements OnInit {
     sortResults$: string;
     images$: any;
     isPaginated$: boolean;
-    isSortable$: boolean
+    isSortable$: boolean;
+    isVisible$: boolean;
     pages$: Array<number>;
     sorts$: Array<string>;
+    currentImage$: object;
+    imageIndex: number;
 
     constructor(private data: DataService) { }
 
@@ -29,11 +32,14 @@ export class GalleryComponent implements OnInit {
             data => this.images$ = data
         );
         this.pages$ = [5, 10, 15, 20];
-        this.sorts$ = ["Select", "Alphabetical", "Date"];
+        this.sorts$ = ["Select", "Alphabetical", "Date: (Newest)"];
         this.paginationResults$ = parseInt(this.resultsPerPage, 10);
         this.sortResults$ = "Select";
         this.isPaginated$ = JSON.parse(this.pagination);
         this.isSortable$ = JSON.parse(this.sortable);
+        this.isVisible$ = false;
+        this.currentImage$ = "";
+        this.imageIndex = 0;
     }
 
     perPageChangeHandler(event: any) {
@@ -50,13 +56,34 @@ export class GalleryComponent implements OnInit {
                     return 0;
                 })
         };
-        if (this.sortResults$ === "Date") {
+        if (this.sortResults$ === "Date: (Newest)") {
             this.images$ =
                 this.images$.sort((left: any, right: any): number => {
-                    if (Date.parse(left.date) < Date.parse(right.date)) return -1;
-                    if (Date.parse(left.date) > Date.parse(right.date)) return 1;
+                    if (Date.parse(left.date) < Date.parse(right.date)) return 1;
+                    if (Date.parse(left.date) > Date.parse(right.date)) return -1;
                     return 0;
                 })
         };
+    }
+
+    showModal(image: object) {
+        this.isVisible$ = !this.isVisible$;
+        this.currentImage$ = image;
+    }
+
+    nextImage(current: object) {
+        this.imageIndex = this.images$.indexOf(current);
+        if (this.imageIndex >= 0 && this.imageIndex < this.images$.length - 1) {
+            this.currentImage$ = this.images$[this.imageIndex + 1];
+        }
+
+    }
+
+    prevImage(current: object) {
+        this.imageIndex = this.images$.indexOf(current);
+        if (this.imageIndex > 0 && this.imageIndex < this.images$.length) {
+            this.currentImage$ = this.images$[this.imageIndex - 1];
+        }
+
     }
 }
